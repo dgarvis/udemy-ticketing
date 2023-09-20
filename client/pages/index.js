d690b1421be290f8466f9bc63ed84571495845d0
Dylan Garvis <dylan@garvis.dev>
2023-09-19 23:29:43 -0400

    Finished section 11 of SSR

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,32 +1,15 @@
 //import React - to trick emacs into JSX mode
-import axios from 'axios';
+import buildClient from '../api/build-client';
 
 const LandingPage = ({ currentUser }) => {
-    console.log(currentUser);
-    
-    /*axios.get('/api/users/currentuser').catch((err) => {
-	console.log(err.message);
-    });*/
-    
-    return <h1>Landing Page</h1>;
+    return currentUser ? <h1>You are Signed In</h1> : <h1>You are not Signed In</h1>
 };
 
-LandingPage.getInitialProps = async ({ req }) => {
-    if (typeof window === 'undefined') {
-	// we are on the server.
-	const { data } = await axios.get(
-	    'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser', {
-		headers: req.headers
-	    }
-	);
-	return data;
-    } else {
-	// we are on the browser.
-	const { data } = await axios.get('/api/users/currentuser');
-	return data;
-    }
-    
-    return {};
+LandingPage.getInitialProps = async (context) => {
+    const client = buildClient(context);
+    const { data } = await client.get('/api/users/currentuser');
+
+    return data;
 };
 
 export default LandingPage;
