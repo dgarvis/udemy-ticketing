50d1d93d9dea0ff044da9362ad2d66dea0f45010
Dylan Garvis <dylan@garvis.dev>
2023-09-25 06:58:16 -0400

    adjsted docker script to pull from env variables

--- create_docker_hub_secret.sh
+++ create_docker_hub_secret.sh
@@ -1,1 +1,1 @@
-kubectl create secret docker-registry docker-hub-secret --docker-server=https://index.docker.io/v1/ --docker-username=$1 --docker-password=$2
+kubectl create secret docker-registry docker-hub-secret --docker-server=https://index.docker.io/v1/ --docker-username=$DOCKER_USER --docker-password=$DOCKER_PAT
